62aa37e1f58c9ffa20d99b29a9caf3c53640bb75
xogur <snsk656@naver.com>
2025-08-20 21:43:58 +0900

    8 20 4

---
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.frontcontroller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Setter
+@Getter
+public class ModelView {
+
+    private String viewName;
+    private Map<String, Object> model = new HashMap<>();
+
+
+    public ModelView(String viewName) {
+        this.viewName = viewName;
+    }
+}
